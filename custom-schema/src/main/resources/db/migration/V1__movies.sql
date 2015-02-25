bd7b6affed0f93e88c3522f3f5b179c585b2a8bd
Artem Prigoda <aprigoda@bcc.ru>
2015-02-25 23:41:05 +0300

    Change the module name to custom-schema-location

--- custom-schema/src/main/resources/db/migration/V1__movies.sql
+++
@@ -1,8 +0,0 @@
-create table movies (
-  id       bigint identity,
-  name     varchar(128) not null,
-  year     int,
-  director varchar(64)  not null,
-  unique (name, year),
-  check (year > 0)
-);
