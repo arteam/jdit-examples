b3f9334395335268f771e285d4ba20ab5857ce77
Artem Prigoda <aprigoda@bcc.ru>
2015-05-07 23:58:44 +0300

    Add more checks

--- custom-schema-location/src/main/resources/db/migration/V1__movies.sql
+++ custom-schema-location/src/main/resources/db/migration/V1__movies.sql
@@ -1,8 +1,10 @@
 create table movies (
-  id       bigint identity,
+  id       bigint identity ,
   name     varchar(128) not null,
   year     int,
   director varchar(64)  not null,
   unique (name, year),
-  check (year > 0)
+  check (year > 0),
+  check (length(name)> 0),
+  check (length(director)> 0)
 );
